45e8561b27f447408a0cc436830079b7ff2a6d31
Pablo Carranza <pcarranza@gmail.com>
2020-12-08 13:58:28 +0100

    Move things to scripts, sql must be pg compatible

---
+++ script.d/populate_postgres.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -EeufCo pipefail
+IFS=$'\t\n'
+
+bash ./waitforit.sh postgres:5432
+
+echo "Creating bootstrapped model"
+psql -h "postgres" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -f db.d/postgres/0.0.1-bootstrap.sql
+
+echo "Applying fingerprint model update"
+psql -h "postgres" -U "${POSTGRES_USER}" -d "${POSTGRES_DB}" -f db.d/postgres/0.1.0-fingerprint.sql
+
+echo "Done creating model"
